7905ce5e275cc48b754a4a20b7952bd6cfc6867f
Uri Garcia <oriol.garcia.moreiras@estudiantat.upc.edu>
2023-02-28 23:42:11 +0100

    Codi actualizat

--- ogm_codi.py
+++ ogm_codi.py
@@ -48,9 +48,6 @@ plt.show()                            # Per mostrar els grafics
 
 
 # Exercici 1
-# Reprodueix l'exemple fent servir diferents freqüències per la sinusoide. Al menys considera 
-# 4 kHz, a banda d'una freqüència pròpia en el marge audible. Comenta els resultats.
-
 # Agafem la freqüència fx = 4000 Hz
 T = 2.5                               
 fm = 8000                              
@@ -66,7 +63,7 @@ Tx = 1/fx
 Ls = int(fm*5*Tx)                           
 
 # Gràfica1 periodes sinusoide
-plt.figure(1)                            
+plt.figure(2)                            
 plt.plot(t[0:Ls], x[0:Ls])                
 plt.xlabel('t en segons')                 
 plt.title('fx= 4000 Hz, 5 periodes de la sinusoide')   
@@ -78,7 +75,7 @@ sd.play(x, fm)
 N = 5000                       
 X = fft(x[0 : Ls], N)           
 k = np.arange(N)                       
-plt.figure(2)                         
+plt.figure(3)                         
 plt.subplot(211)                      
 plt.plot(k,abs(X))                    
 plt.title(f'Transformada del senyal de Ls={Ls} mostres amb DFT de N={N}')   
@@ -105,7 +102,7 @@ Ty = 1/fy
 Ls = int(fm*5*Ty)                          
 
 # Gràfica2 periodes sinusoide
-plt.figure(3)                             
+plt.figure(4)                             
 plt.plot(t[0:Ls], y[0:Ls])                
 plt.xlabel('t en segons')                
 plt.title('fy= 700 Hz,5 periodes de la sinusoide')   
@@ -117,7 +114,7 @@ sd.play(y, fm)
 N=5000                        
 Y=fft(y[0 : Ls], N)           
 k=np.arange(N)                      
-plt.figure(4)                        
+plt.figure(5)                        
 plt.subplot(211)                      
 plt.plot(k,abs(Y))                   
 plt.title(f'Transformada del senyal de Ls={Ls} mostres amb DFT de N={N}')   
@@ -131,10 +128,6 @@ plt.show()
 
 
 # Exercici 2
-# Modifica el programa per considerar com a senyal a analitzar el senyal del fitxer wav que has creat (x_r, fm = sf.read('nom_fitxer.wav')).
-# Insereix a continuació una gràfica que mostri 5 períodes del senyal i la seva transformada.
-# Explica el resultat del apartat anterior.
-
 x_r, fm = sf.read('so_1.wav')     #read del primer fitxer
 Tm = 1/fm
 T = np.arange(len(x_r))                              
@@ -145,7 +138,7 @@ f = fm/2
 T = 1/f                                   # Període del senyal
 Ls = int(fm*5*T)                           # Nombre de mostres corresponents a 5 períodes de la sinusoide
 
-plt.figure(21)                             
+plt.figure(6)                             
 plt.plot(t[0:Ls], x_r[0:Ls])                
 plt.xlabel('t en segons')              
 plt.title('Exercici 2, 5 periodes de la sinusoide')   
@@ -156,7 +149,7 @@ sd.play(x_r, fm)
 N = 5000                       
 X = fft(x_r[0 : Ls], N)         
 k = np.arange(N)                        
-plt.figure(5)                        
+plt.figure(7)                        
 plt.subplot(211)                     
 plt.plot(k,abs(X))                   
 plt.title(f'Transformada del senyal de Ls={Ls} mostres amb DFT de N={N}')   
@@ -169,3 +162,74 @@ plt.show()
 
 
 # Exercici 3
+# Creem i guardem un fitxer un senyal sinusoidal de freqüència fx, digitalitzat a fm, de durada T segons i amplitud A:
+T= 2.5                               
+fm=8000                             
+fx=440                               
+A=4                                  
+pi=np.pi                           
+L = int(fm * T)                     
+Tm=1/fm                              
+t=Tm*np.arange(L)                   
+x = A * np.cos(2 * pi * fx * t)     
+sf.write('nom_fitxerej3.wav', x, fm)  
+Tx=1/fx                                   
+Ls=int(fm*Tx*5)                           
+
+# Gràfica Exercici 3
+plt.figure(8)                             
+plt.plot(t[0:Ls], x[0:Ls])               
+plt.xlabel('t en segons')                 
+plt.title('Exercici 3 (5 períodes)')  
+plt.show()
+sd.play(x, fm)    
+
+# Modul & Fase FFT a partir de 
+N=5000                        
+X=fft(x[0 : Ls], N)        
+k=np.arange(N)                                            
+plt.figure(9)                         
+XdB = 20*np.log10(np.abs(X)/max(np.abs(X))) # Representar en dB
+fk = k[0:N//2+1]*fm/N         # Relació entre els valors de l'índex k i la freqüència en Hz
+plt.subplot(211)   
+plt.plot(fk,XdB[0:N//2+1]) 
+plt.title(f'Transformada del senyal de Ls={Ls} mostres amb DFT de N={N}')   
+plt.ylabel('Mòdul en dB')                  
+plt.subplot(212)                      
+plt.plot(fk,np.unwrap(np.angle(X[0:N//2+1])) )   
+plt.xlabel('f en Hz')                
+plt.ylabel('$\phi_x[k]$')              
+plt.show()     
+
+
+
+# Exercici 4
+T= 0.025                               
+data, fm =sf.read('so_exercici4.wav')       
+L = int(fm * T)                     
+Tm=1/fm                            
+t=Tm*np.arange(L)                  
+sf.write('fitxer_exercici4.wav', data, fm)  
+
+plt.figure(10)                          
+plt.plot(t[0:L],data[0:L])              
+plt.xlabel('t en segons')               
+plt.title('Exercici 4 (5 períodes)')  
+plt.show()                             
+
+
+N=5000                        
+X=fft(data[0 : L], N)    
+k=np.arange(N)                                         
+plt.figure(11)                         
+XdB = 20*np.log10(np.abs(X)/max(np.abs(X)))
+fk = k[0:N//2+1]*fm/N
+plt.subplot(211)   
+plt.plot(fk,XdB[0:N//2+1]) 
+plt.title(f'Transformada del senyal de Ls={L} mostres amb DFT de N={N}')   
+plt.ylabel('Mòdul en dB')                  
+plt.subplot(212)                      
+plt.plot(fk,np.unwrap(np.angle(X[0:N//2+1])) )   
+plt.xlabel('f en Hz')                
+plt.ylabel('$\phi_x[k]$')          
+plt.show() 
